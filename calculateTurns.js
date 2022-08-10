a71c05bc401a04c9225f9b126198134cb79abbbf
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2022-08-11 05:24:56 +0530

    Update calculateTurns.js

--- calculateTurns.js
+++ calculateTurns.js
@@ -1,23 +1,27 @@
 // Function to calculate the turns required
 const calculateTurns = (sourceDirection, xMoves, yMoves) => {
+  const updateTurns = (moves) => {
+    if (moves === 0) {
+      return 0;
+    } else {
+      return 1;
+    }
+  };
 
-    let turnsRequired = 2;
-    if((sourceDirection === 'E' && xMoves >= 0) || (sourceDirection === 'W' && xMoves <= 0)){
-        if(yMoves === 0){
-            turnsRequired = 0
-        }else{
-            turnsRequired -= 1
-        }
-    }
-    if((sourceDirection === 'N' && yMoves >= 0) || (sourceDirection === 'S' && yMoves <= 0)){
-        if(xMoves === 0){
-            turnsRequired = 0
-        }else{
-            turnsRequired -= 1
-        }
-    }
-    return turnsRequired;
-    
+  let turnsRequired = 2;
+  if (
+    (sourceDirection === "E" && xMoves >= 0) ||
+    (sourceDirection === "W" && xMoves <= 0)
+  ) {
+    turnsRequired = updateTurns(yMoves);
+  }
+  if (
+    (sourceDirection === "N" && yMoves >= 0) ||
+    (sourceDirection === "S" && yMoves <= 0)
+  ) {
+    turnsRequired = updateTurns(xMoves);
+  }
+  return turnsRequired;
 };
 
 module.exports = calculateTurns;
